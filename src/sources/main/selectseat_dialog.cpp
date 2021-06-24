439800e79677edc6899e42736ab1df3e1f47a99a
mengxing <407692831@qq.com>
2021-06-24 12:50:23 +0800

    重构目录结构

---
+++ src/sources/main/selectseat_dialog.cpp
@@ -0,0 +1,137 @@
+#include "src/include/main/selectseat_dialog.h"
+#include "src/include/ui/ui_selectseat_dialog.h"
+
+#include <QDebug>
+#include <QMessageBox>
+#include <QStandardItemModel>
+#include <QTreeWidget>
+#include <QTreeWidgetItem>
+SelectSeat_Dialog::SelectSeat_Dialog(QWidget *parent)
+    : QDialog(parent), ui(new Ui::SelectSeat_Dialog) {
+  ui->setupUi(this);
+  this->setWindowTitle("请选择座位");
+  orderDishes = new OrderDishesDialog;
+
+  idData = ""; //初始化数据
+  m_db = QSqlDatabase::addDatabase("QSQLITE");
+  m_db.setDatabaseName("data.db");
+  if (!m_db.open()) {
+    qDebug() << "Error: connection with database fail";
+  } else {
+    qDebug() << "Database: connection ok";
+  }
+  initWindow();
+  deskInit();
+}
+
+SelectSeat_Dialog::~SelectSeat_Dialog() {
+  delete orderDishes;
+  delete ui;
+}
+
+void SelectSeat_Dialog::initWindow() {
+  //设置桌号显示
+  QBrush myBrush;
+  QPalette palette;
+  myBrush = QBrush(Qt::red, Qt::DiagCrossPattern);
+  palette.setBrush(QPalette::Text, myBrush);
+  ui->lineEdit_Id->setEnabled(false);
+  ui->lineEdit_Id->setPalette(palette);
+
+  //设置表格显示
+  ui->tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
+  ui->tableWidget->setSelectionBehavior(
+      QAbstractItemView::SelectRows); //选中整行
+  // ui->tableWidget->item(0,0)->setTextAlignment(Qt::AlignHCenter);//选择对齐方式
+  ui->tableWidget->setAlternatingRowColors(true); //设置每行变一色
+  ui->tableWidget->setColumnCount(2);
+  QStringList list;
+  list << "桌号"
+       << "空闲状态";
+  ui->tableWidget->setHorizontalHeaderLabels(list);
+  ui->tableWidget->setColumnWidth(0, 160);
+  ui->tableWidget->setColumnWidth(1, 180);
+  //设置列名为加粗
+  QFont font = ui->tableWidget->horizontalHeader()->font();
+  font.setBold(true);
+  ui->tableWidget->horizontalHeader()->setFont(font);
+  //去掉第一列标号
+  QHeaderView *header = ui->tableWidget->verticalHeader();
+  header->setHidden(true);
+  //设置选中背景色
+  ui->tableWidget->setStyleSheet("selection-background-color:lightblue;");
+  ui->tableWidget->setSelectionMode(
+      QAbstractItemView::SingleSelection); //设置为可以选中单个
+}
+
+void SelectSeat_Dialog::on_button_Ok_clicked() {
+  if ("有人" == flagId) {
+    QMessageBox::information(this, "温馨提示", "此座位有人,请重新选择座位");
+    return;
+  }
+  if (NULL != idData) {
+    orderDishes->getDatabase(m_db);
+    orderDishes->setModal(true);
+    orderDishes->initFoodTable();
+    orderDishes->receiverIdData(idData);
+    orderDishes->show();
+    orderDishes->exec();
+    bool isLeisure = orderDishes->returnIsOrder();
+    qDebug() << isLeisure;
+    //将数据插入数据库
+    if (isLeisure) {
+      qDebug() << idData;
+
+      QString time = orderDishes->returnTime();
+
+      QString idTable = ui->lineEdit_Id->text();
+      QSqlQuery query(m_db);
+      qDebug() << idData;
+      //数据更新操作
+      if (query.exec("update TableInfo set isUse = '有人',time='" + time +
+                     "'   where TableID ='" + idTable + "'"))
+        qDebug() << "打开成功";
+      deskInit();
+    }
+    ui->lineEdit_Id->clear();
+    return;
+  }
+  QMessageBox::information(this, "温馨提示", "请选择座位");
+}
+
+void SelectSeat_Dialog::on_button_Cancel_clicked() { this->close(); }
+
+void SelectSeat_Dialog::on_tableWidget_clicked(const QModelIndex &index) {
+  // index的警告无需关注
+  ui->lineEdit_Id->clear();
+  int row = ui->tableWidget->currentIndex().row();
+  QAbstractItemModel *model = ui->tableWidget->model();
+  QModelIndex indextemp = model->index(row, 0);
+  QModelIndex indexflag = model->index(row, 1);
+  idData = model->data(indextemp).toString();
+  flagId = model->data(indexflag).toString();
+  ui->lineEdit_Id->setText(idData);
+}
+
+void SelectSeat_Dialog::deskInit() {
+
+  QSqlQuery query(m_db);
+  query.exec("select *from TableInfo");
+  query.last();
+  int row = query.value(0).toInt();
+  qDebug() << row;
+  ui->tableWidget->setRowCount(row);
+  query.first();
+  query.previous();
+  int i = 0;
+  while (query.next()) {
+    QString deskId = query.value(1).toString();
+    qDebug() << query.value(2).toString();
+    QString flag = query.value(2).toString();
+    ui->tableWidget->setItem(i, 0, new QTableWidgetItem(deskId));
+    ui->tableWidget->setItem(i, 1, new QTableWidgetItem(flag));
+    i++;
+  }
+}
+
+void SelectSeat_Dialog::on_button_Close_clicked() { this->close(); }
---
+++ src/sources/ui/ui_dayincomequery_dialog.cpp
@@ -0,0 +1,83 @@
+#include "src/include/ui/ui_dayincomequery_dialog.h"
+
+void Ui_DayIncomeQuery_Dialog::setupUi(QDialog *DayIncomeQuery_Dialog) {
+  if (DayIncomeQuery_Dialog->objectName().isEmpty())
+    DayIncomeQuery_Dialog->setObjectName(
+        QStringLiteral("DayIncomeQuery_Dialog"));
+  DayIncomeQuery_Dialog->resize(600, 500);
+  QPalette dayIncomeQuery_Pal(DayIncomeQuery_Dialog->palette());
+  dayIncomeQuery_Pal.setBrush(
+      QPalette::Background,
+      QBrush(QPixmap(":/icon/res/images/selectSeat_background.jpeg")));
+  DayIncomeQuery_Dialog->setAutoFillBackground(true);
+  DayIncomeQuery_Dialog->setPalette(dayIncomeQuery_Pal);
+
+  queryButton = new QPushButton(DayIncomeQuery_Dialog);
+  queryButton->setObjectName(QStringLiteral("queryButton"));
+  queryButton->setGeometry(QRect(40, 60, 80, 22));
+  quitButton = new QPushButton(DayIncomeQuery_Dialog);
+  quitButton->setObjectName(QStringLiteral("quitButton"));
+  quitButton->setGeometry(QRect(150, 60, 80, 22));
+  widget = new QWidget(DayIncomeQuery_Dialog);
+  widget->setObjectName(QStringLiteral("widget"));
+  widget->setGeometry(QRect(30, 20, 359, 24));
+  horizontalLayout = new QHBoxLayout(widget);
+  horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+  horizontalLayout->setContentsMargins(0, 0, 0, 0);
+  label = new QLabel(widget);
+  label->setObjectName(QStringLiteral("label"));
+
+  horizontalLayout->addWidget(label);
+
+  year_comboBox = new QComboBox(widget);
+  year_comboBox->setObjectName(QStringLiteral("year_comboBox"));
+
+  horizontalLayout->addWidget(year_comboBox);
+
+  label_2 = new QLabel(widget);
+  label_2->setObjectName(QStringLiteral("label_2"));
+
+  horizontalLayout->addWidget(label_2);
+
+  month_comboBox = new QComboBox(widget);
+  month_comboBox->setObjectName(QStringLiteral("month_comboBox"));
+
+  horizontalLayout->addWidget(month_comboBox);
+
+  label_3 = new QLabel(widget);
+  label_3->setObjectName(QStringLiteral("label_3"));
+
+  horizontalLayout->addWidget(label_3);
+
+  day_comboBox = new QComboBox(widget);
+  day_comboBox->setObjectName(QStringLiteral("day_comboBox"));
+
+  horizontalLayout->addWidget(day_comboBox);
+
+  label_4 = new QLabel(widget);
+  label_4->setObjectName(QStringLiteral("label_4"));
+
+  horizontalLayout->addWidget(label_4);
+
+  retranslateUi(DayIncomeQuery_Dialog);
+
+  QMetaObject::connectSlotsByName(DayIncomeQuery_Dialog);
+} // setupUi
+
+void Ui_DayIncomeQuery_Dialog::retranslateUi(QDialog *DayIncomeQuery_Dialog) {
+  DayIncomeQuery_Dialog->setWindowTitle(
+      QApplication::translate("DayIncomeQuery_Dialog", "Dialog", nullptr));
+  queryButton->setText(QApplication::translate(
+      "DayIncomeQuery_Dialog", "\346\237\245\350\257\242", nullptr));
+  quitButton->setText(QApplication::translate(
+      "DayIncomeQuery_Dialog", "\351\200\200\345\207\272", nullptr));
+  label->setText(QApplication::translate(
+      "DayIncomeQuery_Dialog",
+      "\351\200\211\346\213\251\346\227\245\346\234\237", nullptr));
+  label_2->setText(QApplication::translate("DayIncomeQuery_Dialog",
+                                           "\345\271\264", nullptr));
+  label_3->setText(QApplication::translate("DayIncomeQuery_Dialog",
+                                           "\346\234\210", nullptr));
+  label_4->setText(QApplication::translate("DayIncomeQuery_Dialog",
+                                           "\346\227\245", nullptr));
+} // retranslateUi
