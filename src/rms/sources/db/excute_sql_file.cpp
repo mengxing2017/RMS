5fbe9622820f01f3b04cb546f7583bb80b3c6256
mengxing <407692831@qq.com>
2021-07-03 11:13:18 +0800

    删除不必要的调试代码

--- src/rms/sources/db/excute_sql_file.cpp
+++ src/rms/sources/db/excute_sql_file.cpp
@@ -5,38 +5,36 @@
 
 ExcuteSqlFile::ExcuteSqlFile() {}
 
-int ExcuteSqlFile::excute(QFile *file, QSqlDatabase db) {
+bool ExcuteSqlFile::excute(QFile *file, QSqlDatabase db) {
   Log4Qt::Logger *log = Log::createLog()->getLogger();
-  int iRet = 0;
+
   if (!file->exists()) {
-    return -1;
+    log->error("sql file not exist！");
+    return false;
   }
   if (!file->open(QIODevice::ReadOnly | QIODevice::Text)) {
-    return -1;
+    log->error("sql file open failed！");
+    return false;
   }
   if (db.isOpen()) {
-    qDebug() << "excuteSqlFile: open database....";
     db.open();
   }
+
   QTextStream in(file);
   in.setCodec("UTF-8");
   QString sqlData = in.readAll();
   QSqlQuery query(db);
   QStringList qstrlist_sql = sqlData.split(";");
-  log->debug(QString::number(qstrlist_sql.size()));
-  for (int i = 0; i < qstrlist_sql.size() - 1; i++) {
-    QString qstr_sql_part = qstrlist_sql.at(i).toUtf8();
-    log->debug(qstr_sql_part);
-    bool success = query.exec(qstr_sql_part);
-    if (!success) {
-      QSqlError lastError = query.lastError();
-      QString err = lastError.driverText();
-      log->error(err);
-      iRet = -1;
-      break;
-    }
-    log->debug("创建数据库表成功！");
+
+  QString qstr_sql_part = qstrlist_sql.at(0).toUtf8();
+  log->debug(qstr_sql_part);
+  bool success = query.exec(qstr_sql_part);
+  if (!success) {
+    QSqlError lastError = query.lastError();
+    QString err = lastError.driverText();
+    log->error(err);
+    return false;
   }
 
-  return iRet;
+  return true;
 }
